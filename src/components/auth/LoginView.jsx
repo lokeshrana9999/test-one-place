30c4e901ae784e7eba2620323f504e3d421dfeca
Lokesh RANA <lokeshrana9999@gmail.com>
2021-01-26 17:43:43 +0530

    Update LoginView.jsx

--- src/components/auth/LoginView.jsx
+++ src/components/auth/LoginView.jsx
@@ -100,7 +100,7 @@ const LoginView = (props) => {
         setAccessTokene(sending.accessToken);
         setRefreshTokene(sending.refreshToken);
         const params = queryString.parse(history.location.search);
-        history.push(params.redirectBack ? params.redirectBack : "/");
+        history.push(params.redirectBack ? params.redirectBack : "/profile");
       } else {
         message.error({
           duration: 2,
